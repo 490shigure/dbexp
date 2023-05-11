629ba71586b34bf3e855ab73b561b8a4136e8af7
kotlyne <kotlyne@outlook.com>
2023-05-11 19:00:38 +0800

    添加pinia

--- src/renderer/src/views/Home.vue
+++ src/renderer/src/views/Home.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import { useCounterStore } from '@renderer/stores/counter'
 
 const router = useRouter()
 const onLoginCick = () => {
   router.push('/login')
 }
+
+const counter = useCounterStore()
 </script>
 
 <template>
   <p>Hello World from Home</p>
+  <p>count value{{ counter.count }}</p>
   <button @click="onLoginCick">去登陆</button>
+  <button @click="counter.increment">+1</button>
 </template>
 
 <style scoped></style>
