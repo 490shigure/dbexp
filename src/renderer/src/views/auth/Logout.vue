bc41cbd5d9e48a6da2e84298121a659afebabc88
kotlyne <kotlyne@outlook.com>
2023-05-12 22:16:41 +0800

    chore: 修复登出界面按钮文字

--- src/renderer/src/views/auth/Logout.vue
+++ src/renderer/src/views/auth/Logout.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 <template>
   <ElResult icon="success" title="登出成功" sub-title="按下按钮返回登录界面">
     <template #extra>
-      <ElButton type="primary" @click="router.push('/auth/login')">Back</ElButton>
+      <ElButton type="primary" @click="router.push('/auth/login')">登录</ElButton>
     </template>
   </ElResult>
 </template>
